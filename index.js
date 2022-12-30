897d6cb4541c501263dacdff5ea8d519f6a46e82
Mohamed Saber <mohamed.71.saber@gmail.com>
2022-12-30 21:46:13 +0200

    linked with front end

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express();
 const PORT = 3001;
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
-//app.use(-
-//cors({
-//origin: ["http://localhost:3001/registration"],
-//})
-//);
+//https://expa-server.onrender.com/adminSignin
+
+app.use(cors({
+  origin:["https://harmonious-donut-522662.netlify.app", "https://harmonious-donut-522662.netlify.app/sign-in"]
+}));
 app.use(router);
 const uri =
   "mongodb+srv://M_Saber:jHokLaTlZQnEbA8C@cluster0.xaphthb.mongodb.net/?retryWrites=true&w=majority";
--- src/models/donation.js
+++ src/models/donation.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose");
 
-
 const donationSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required:true
-    },
-    phoneNumber:{
-      type:String,
-      required:true
-    },
-    amount: {
-        type: String,
-        required:true
-    }
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Donation", donationSchema);
