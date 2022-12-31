1c2925907c852a00326039de5a2cf59927ec493a
Mohamed Saber <mohamed.71.saber@gmail.com>
2022-12-31 14:45:06 +0200

    added authentication for admin and user

--- src/controllers/getAdmin.js
+++ src/controllers/getAdmin.js
@@ -1,10 +1,29 @@
-const Admin = require("../models/admin");
+const Volunteer = require("../models/volunteer");
 
 const getAdmin = async (req, res) => {
   const body = req.body;
-  const admin = await Admin.find({}).exec();
+  const email = body.email;
+  const password = body.password;
 
-  res.json(admin);
+  try {
+    // Find the user with the specified email and password
+    const users = await Volunteer.find({ email: email, password: password }).exec();
+    if (!users.length) {
+      // Return an error if the user is not found
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    // Check the user's role field to determine their status
+    const user = users[0];
+    if (user.role === "admin") {
+      res.send({ status: "admin" });
+    } else {
+      res.send({ status: "user" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "An error occurred while retrieving the user" });
+  }
 };
 
 module.exports = getAdmin;
--- src/controllers/getUser.js
+++ src/controllers/getUser.js
@@ -3,7 +3,7 @@ const Volunteer = require("../models/volunteer");
 const getUser = async (req, res) => {
   const body = req.body;
   const volunteer = await Volunteer.find(
-    {},
+    { role: { $ne: "admin" } },
     { name: 1, phoneNumber: 1, email: 1 }
   ).exec();
 
