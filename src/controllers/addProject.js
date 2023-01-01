2a7c511c8778c3e75a5c3b5afca027ebc5ff0ac1
Mohamed Saber <mohamed.71.saber@gmail.com>
2023-01-01 20:20:53 +0200

    added comments

--- src/controllers/addProject.js
+++ src/controllers/addProject.js
@@ -1,20 +1,24 @@
-const Project=require("../models/project");
+const Project = require("../models/project");
 
-const addProject = async(req,res)=>{
-    const body=req.body;
-    const newProject = new Project({
-        ProjectName: body.ProjectName,
-        ProjectDescription: body.ProjectDescription,
-        ProjectFees: body.ProjectFees,
-        ProjectDate:body.ProjectDate,
-        timestamp:new Date(),
-        country: body.country,
-      
+// Async function that adds a new project to the database
+const addProject = async (req, res) => {
+  // Get the request body
+  const body = req.body;
+  // Create a new Project document using the request body
+  const newProject = new Project({
+    ProjectName: body.ProjectName,
+    ProjectDescription: body.ProjectDescription,
+    ProjectFees: body.ProjectFees,
+    ProjectDate: body.ProjectDate,
+    // Set the timestamp field to the current date and time
+    timestamp: new Date(),
+    country: body.country,
+  });
+  // Save the new document to the Project collection in the database
+  await newProject.save();
+  // Send a response with a success message
+  res.send("Project Added");
+};
 
-        
-    });
-    await newProject.save();
-    res.send('Project Added');
-}
-
-module.exports=addProject;
+// Export the addProject function
+module.exports = addProject;
